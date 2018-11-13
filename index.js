9910de883e316399311d3b98661e13a794bebc07
Dante <gabriel.n64@hotmail.com>
2018-11-13 14:56:45 -0200

    configure cors, add webpeople

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const app = require("express")();
 
 const consign = require("consign");
 
-const db = require('./config/db');
-
+const db = require("./config/db");
 
 app.db = db;
 
 const port = process.env.PORT || 3000;
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.webpeople.net.br");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, OPTIONS"
+  );
+  next();
+});
 
 consign()
   .include("./config/passport.js")
   .then("./config/middlewares.js")
-  .then('./components/validation.js')
+  .then("./components/validation.js")
   .then("./components/blogwp/blogwp.js")
   .then("./components/blogwp")
   .then("./config/routes.js")
