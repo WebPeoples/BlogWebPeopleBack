46234eb9571afdf23b69ba77909c9fcbbd58d5bd
Dante <gabriel.n64@hotmail.com>
2018-11-13 15:08:58 -0200

    configure cors, add webpeople

--- index.js
+++ index.js
@@ -7,17 +7,13 @@ const db = require("./config/db");
 app.db = db;
 
 const port = process.env.PORT || 3000;
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, OPTIONS"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 });
 
 consign()
