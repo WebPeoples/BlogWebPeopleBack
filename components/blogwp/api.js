244e0e5e2c78afc4d3740f02ac5c17336ec2f27c
Dante <gabriel.n64@hotmail.com>
2018-11-13 15:42:20 -0200

    .env inserido

--- components/blogwp/api.js
+++ components/blogwp/api.js
@@ -35,7 +35,7 @@ module.exports = app => {
   };
 
   const ListaArtigos = (req, res) => {
-    await app
+    app
       .db("articles")
       .then(articles => res.json(articles))
       .catch(err => res.status(500).send(err));
